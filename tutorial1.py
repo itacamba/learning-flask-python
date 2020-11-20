32b181dde037dd68c8a723e71bc0b5a6b09c4356
itacamba <daniela.sahagun03@gmail.com>
2020-11-20 00:24:01 -0500

    learned to route and return pages:

---
+++ tutorial1.py
@@ -0,0 +1,17 @@
+from flask import Flask, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "homepage"
+
+@app.route('/<name>')
+def user(name):
+    return "hello %s" % name
+
+@app.route('/admin')
+def admin():
+    return redirect(url_for('user'))
+if __name__ == "__main__":
+    app.run()
