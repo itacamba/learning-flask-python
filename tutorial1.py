b3d155f7e80bbd34ab4dae7c631d75fe394b627d
itacamba <daniela.sahagun03@gmail.com>
2020-11-21 15:20:36 -0500

    learning how to iterate through array and render conditionally

--- tutorial1.py
+++ tutorial1.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/<name>')
 def home(name):
-    return render_template("index.html")
+    return render_template("index.html", content = ['Tim', 'Joe', 'Bill'])
 
 
 if __name__ == "__main__":
